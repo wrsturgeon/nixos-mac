0102f1e489c936077e776a1bd3727ae0bb56b6b9
Will Sturgeon <willstrgn@gmail.com>
2023-06-26 01:04:20 -0700

    Automatic update with `rebuild.sh`

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: let
   flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
   hyprland = (import flake-compat {
-    src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz"
+    src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
   }).defaultNix;
 in {
   boot.loader = {
