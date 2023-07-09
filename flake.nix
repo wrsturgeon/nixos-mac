f1ada910419166e42d6e3e1fcc3ad06c2dc75f3a
Will Sturgeon <willstrgn@gmail.com>
2023-07-09 02:57:25 -0700

    Automatic update with `rebuild.sh`

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: {
-    nixosConfigurations."macbook-nixos" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.macbook-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
